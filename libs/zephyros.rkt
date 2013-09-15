ed8d8fd9a7f183f81bdf30cacac477caaf4f089c
Shriphani Palakodety <shriphani@Shriphanis-MacBook-Air.local>
2013-09-15 04:56:15 -0400

    read/write of some initial routines in racket

---
+++ libs/zephyros.rkt
@@ -0,0 +1,78 @@
+#lang racket
+
+(require json
+         racket/future
+         racket/place)
+
+(require (planet shawnpresser/racket-unix-sockets:1:0))
+
+(provide clipboard-contents
+         focused-window
+         visible-windows
+         all-windows
+         main-screen
+         all-screens
+         running-apps)
+
+
+(define-values
+  (i o)
+  (unix-socket-connect "/tmp/zephyros.sock"))
+
+(define next-msg-id 0)
+
+(define (get-next-msg-id)
+  (let ((to-return next-msg-id))
+    (set! next-msg-id (+ next-msg-id 1))
+    to-return))
+
+(define (send-message message)
+  (displayln (message-str->json-str message) o)
+  (flush-output o))
+
+(define (reader)
+  (define (loop)
+    (print (read-message))
+    (loop))
+  (loop))
+
+(define (read-message)
+  (second (read-json i)))
+
+(define (message-str->json-str message [args '()])
+  (jsexpr->string (append
+                   (list
+                    (get-next-msg-id)
+                    0
+                    message)
+                   args)))
+
+;; routines that just return values and don't change
+;; state
+(define (clipboard-contents)
+  (send-message "clipboard_contents")
+  (read-message))
+
+(define (focused-window)
+  (send-message "focused_window")
+  (read-message))
+
+(define (visible-windows)
+  (send-message "visible_windows")
+  (read-message))
+
+(define (all-windows)
+  (send-message "all_windows")
+  (read-message))
+
+(define (main-screen)
+  (send-message "main_screen")
+  (read-message))
+
+(define (all-screens)
+  (send-message "all_screens")
+  (read-message))
+
+(define (running-apps)
+  (send-message "running_apps")
+  (read-message))
