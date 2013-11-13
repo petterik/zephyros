96ba1980eeb778bc36c9b9abbc6fc88b7a6c6a4d
Steven Degutis <steven@cleancoders.com>
2013-11-13 11:57:32 -0600

    Apparently Apple made it impossible to support both 10.8 and 10.9 to nicely ask the user to turn on accessibility, so we have to pick one.

--- Zephyros/SDUniversalAccessHelper.m
+++ Zephyros/SDUniversalAccessHelper.m
@@ -11,74 +11,20 @@
 @implementation SDUniversalAccessHelper
 
 + (BOOL) complainIfNeeded {
-    Boolean enabled = AXAPIEnabled();
+    static BOOL shouldAsk = YES;
     
-    //for 10.8 and older
-    NSString *titlePreMavericks = @"Zephyros requires Universal Access";
-    NSString *messagePreMavericks =
-        @"For Zephyros to function properly, access for assistive devices must "
-         "be enabled first.\n\nTo enable this feature, click \"Enable access for "
-         "assistive devices\" in the Universal Access pane of System Preferences.";
-    NSString *actionButtonTextPreMavericks = @"Open Universal Access";
-    NSString *panePreMavericks = @"com.apple.preference.universalaccess";
-
-    //for 10.9 and newer
-    NSString *titleMavericksOn = @"Zephyros requires Accessibility control";
-    NSString *messageMavericksOn =
-        @"For Zephyros to function properly, it must first be allowed to control "
-         "your computer.\n\nTo enable this feature, unlock the Privacy tab of "
-         "the Security & Privacy pane of System Preferences and check the box "
-         "next to Zephyros.";
-    NSString *actionButtonTextMavericksOn = @"Open Privacy";
-    NSString *paneMavericksOn = @"com.apple.preference.security";
-
-    //same for both
-    NSString *dismissButtonText = @"Dismiss";
-
-    //The replacement is the pane to use
-    NSString *appleScriptFormat =
-      @"tell application \"System Preferences\"\nactivate\nset current pane to pane \"%@\"\nend tell";
-
-    NSString *title, *message, *actionButtonText, *appleScript;
-
-    if([self olderThanMavericks]){
-        title = titlePreMavericks;
-        message = messagePreMavericks;
-        actionButtonText = actionButtonTextPreMavericks;
-        appleScript = [NSString stringWithFormat: appleScriptFormat, panePreMavericks];
-    } else {
-        title = titleMavericksOn;
-        message = messageMavericksOn;
-        actionButtonText = actionButtonTextMavericksOn;
-        appleScript = [NSString stringWithFormat: appleScriptFormat, paneMavericksOn];
-
-    }
-
-    if (!enabled) {
-        [NSApp activateIgnoringOtherApps:YES];
-        
-        NSInteger result = NSRunAlertPanel(title, message, actionButtonText, dismissButtonText, nil);
-        
-        if (result == NSAlertDefaultReturn) {
-            NSAppleScript *a = [[NSAppleScript alloc] initWithSource: appleScript];
-            [a executeAndReturnError:nil];
-        }
-        
-        return YES;
+    BOOL enabled = AXIsProcessTrustedWithOptions((__bridge CFDictionaryRef)@{(__bridge id)kAXTrustedCheckOptionPrompt: @(shouldAsk)});
+    
+    if (shouldAsk) {
+        shouldAsk = NO;
+        double delayInSeconds = 5.0;
+        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+            shouldAsk = YES;
+        });
     }
     
-    return NO;
-}
-
-// NO if running newer than OSX 10.8
-+ (BOOL) olderThanMavericks {
-    SInt32 majorVersion, minorVersion;
-
-    if(Gestalt(gestaltSystemVersionMajor, &majorVersion) == noErr &&
-       Gestalt(gestaltSystemVersionMinor, &minorVersion) == noErr)
-        if((majorVersion == 10 && minorVersion >= 9 ) || majorVersion > 10)
-            return NO;
-    return YES;
+    return !enabled;
 }
 
 @end
